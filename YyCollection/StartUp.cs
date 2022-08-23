02d896a7e504298dae722aeb47581fc651e3eb0d
eyedrops <eyedropsp@gmail.com>
2022-08-23 23:58:18 +0900

    WebApi プロジェクト追加

---
+++ YyCollection/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace YyCollectionForTwitch;
+
+public class StartUp
+{
+    public IConfiguration Configuration { get; }
+
+    public StartUp(IConfiguration configuration)
+        => this.Configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(static endpoints =>
+            endpoints.MapControllerRoute(name: "default", pattern: "{controller=Todos}/{action=Index}/{id?}"));
+    }
+}
