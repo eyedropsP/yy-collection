17c23fbe1618959970fdaea3158b378a9aebadcd
eyedrops <eyedropsp@gmail.com>
2022-08-28 03:23:53 +0900

    コントローラー、レスポンス追加

--- src/YyCollection.Server/Controllers/PostController.cs
+++ src/YyCollection.Server/Controllers/PostController.cs
@@ -1,10 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YyCollection.Server.DomainService.Posts;
 
 namespace YyCollection.Server.Controllers;
 
+/// <summary>
+/// 投稿関連の API を提供します。
+/// </summary>
 [ApiController]
-[Route("[controller]")]
-public class PostController : ControllerBase
+[Route("api/v{version:apiVersion}/[controller]")]
+public class PostsController : ControllerBase
 {
-    
+    /// <summary>
+    /// 指定された投稿を取得します。
+    /// </summary>
+    /// <param name="postService"></param>
+    /// <param name="postId"></param>
+    /// <returns></returns>
+    [AllowAnonymous]
+    [HttpGet]
+    [Route("{postId}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async ValueTask<IActionResult> GetPostById(
+        [FromServices] PostService postService,
+        [FromRoute] Ulid postId)
+    {
+        return this.Ok();
+    }
+
+
+    /// <summary>
+    /// 投稿を指定された条件で取得します。
+    /// </summary>
+    /// <param name="postService"></param>
+    /// <param name="limit"></param>
+    /// <param name="offset"></param>
+    /// <returns></returns>
+    [AllowAnonymous]
+    [HttpGet]
+    [Route("")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async ValueTask<IActionResult> GetPosts(
+        [FromServices] PostService postService,
+        int limit,
+        int offset)
+    {
+        return this.Ok();
+    }
 }
---
+++ src/YyCollection.Server/Models/Posts/PostResponse.cs
@@ -0,0 +1,120 @@
+﻿using System.Text.Json.Serialization;
+
+namespace YyCollection.Server.Models.Posts;
+
+/// <summary>
+/// /Post/ の API レスポンスを表します。
+/// </summary>
+public sealed class PostResponse
+{
+    #region プロパティ
+    /// <summary>
+    /// 投稿 ID
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("id")]
+    public Ulid Id { get; init; }
+    
+    
+    /// <summary>
+    /// 投稿タイトル
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("title")]
+    public string Title { get; init; }
+    
+    
+    /// <summary>
+    /// 投稿者 ID
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("contributorId")]
+    public Ulid ContributorId { get; init; }
+    
+    
+    /// <summary>
+    /// 投稿者アイコン URL
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("contributorIconUrl")]
+    public string ContributorIconUrl { get; init; }
+    
+    
+    /// <summary>
+    /// 投稿者名
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("contributorName")]
+    public string ContributorName { get; init; }
+    
+    
+    /// <summary>
+    /// 投稿説明
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("description")]
+    public string Description { get; init; }
+
+
+    /// <summary>
+    /// 元動画 URL
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("mediaUrl")]
+    public string MediaUrl { get; init; }
+    
+    
+    /// <summary>
+    /// シーク開始位置 [s]
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("startTime")]
+    public int StartTime { get; init; }
+    
+    
+    /// <summary>
+    /// シーク終了位置 [s]
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("endTime")]
+    public int EndTime { get; init; }
+
+
+    /// <summary>
+    /// 投稿日
+    /// </summary>
+    [JsonInclude]
+    [JsonPropertyName("postDate")]
+    public DateOnly PostDate { get; init; }
+    #endregion
+
+
+    #region コンストラクタ
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="title"></param>
+    /// <param name="contributorId"></param>
+    /// <param name="contributorName"></param>
+    /// <param name="contributorIconUrl"></param>
+    /// <param name="description"></param>
+    /// <param name="mediaUrl"></param>
+    /// <param name="startTime"></param>
+    /// <param name="endTime"></param>
+    /// <param name="postDate"></param>
+    public PostResponse(Ulid id, string title, Ulid contributorId, string contributorName, string contributorIconUrl, string description, string mediaUrl, int startTime, int endTime, DateOnly postDate)
+    {
+        this.Id = id;
+        this.Title = title;
+        this.ContributorId = contributorId;
+        this.ContributorName = contributorName;
+        this.ContributorIconUrl = contributorIconUrl;
+        this.Description = description;
+        this.MediaUrl = mediaUrl;
+        this.StartTime = startTime;
+        this.EndTime = endTime;
+        this.PostDate = postDate;
+    }
+    #endregion
+}
